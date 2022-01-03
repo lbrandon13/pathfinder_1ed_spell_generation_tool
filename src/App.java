b67d1586ae68e67c352acfdd07957ec5fc71af33
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2022-01-03 10:01:32 +0000

    Created initial Character class with constructor

--- src/App.java
+++ src/App.java
@@ -2,4 +2,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    class Character {
+        
+        private int id;
+        private int casting_stat_value;
+        private String character_class_value;
+        private int character_level_value;
+        private int[][] spell_list;
+
+        public Character (String name, int casting_stat, String character_class, int character_level) {
+            
+            id = Integer.parseInt(name);
+            casting_stat_value = casting_stat;
+            character_class_value = character_class;
+            character_level_value = character_level;
+        }
+
+    }
 }
