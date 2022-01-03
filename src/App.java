dcc4ab1ab881579260a45f85b913e8d29b605220
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2022-01-03 10:18:31 +0000

    Added getter methods for Character class

--- src/App.java
+++ src/App.java
@@ -3,6 +3,11 @@ import java.io.ObjectInputStream.GetField;
 public class App {
     public static void main(String[] args) throws Exception {
         Character testCharacter = new Character("test", 18, "wizard", 5);
+
+        System.out.println(testCharacter.getId());
+        System.out.println(testCharacter.getCastingStatValue());
+        System.out.println(testCharacter.getCharacterClassValue());
+        System.out.println(testCharacter.getCharacterLevelValue());
     }
 
     static class Character {
@@ -15,11 +20,32 @@ public class App {
 
         public Character(String name, int casting_stat, String character_class, int character_level) {
             
-            id = Integer.parseInt(name);
+            // in time replace with function to turn string value to int to form unique identifier
+            try {
+                id = Integer.parseInt(name);
+            }
+            catch (NumberFormatException e) {
+                id = 0;
+            }
             castingStatValue = casting_stat;
             characterClassValue = character_class;
             characterLevelValue = character_level;
         }
 
+        public int getId() {
+            return id;
+        }
+
+        public int getCastingStatValue() {
+            return castingStatValue;
+        }
+
+        public String getCharacterClassValue() {
+            return characterClassValue;
+        }
+
+        public int getCharacterLevelValue() {
+            return characterLevelValue;
+        }
     }
 }
